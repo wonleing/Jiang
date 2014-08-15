01d0740caf1c96af237465677c7281f46859d277
LeonMathilda <506856123@qq.com>
2014-08-15 19:17:08 +0800

    @“主界面修改”

--- newsPicks/cell/NPTimeOnlineCell_iPad.h
+++ newsPicks/cell/NPTimeOnlineCell_iPad.h
@@ -25,6 +25,21 @@
 @property (weak, nonatomic) IBOutlet UIButton *pickBtn5;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel5;
 @property (weak, nonatomic) IBOutlet UIView *mContentView;
+@property (weak, nonatomic) IBOutlet UILabel *title0;
+@property (weak, nonatomic) IBOutlet UIView *avatarview0;
+@property (weak, nonatomic) IBOutlet UILabel *title1;
+@property (weak, nonatomic) IBOutlet UIView *avatarview1;
+@property (weak, nonatomic) IBOutlet UILabel *title2;
+@property (weak, nonatomic) IBOutlet UIView *avatarview2;
+@property (weak, nonatomic) IBOutlet UILabel *title3;
+@property (weak, nonatomic) IBOutlet UIView *avatarview3;
+@property (weak, nonatomic) IBOutlet UILabel *title4;
+@property (weak, nonatomic) IBOutlet UIView *avatarview4;
+@property (weak, nonatomic) IBOutlet UILabel *time0;
+@property (weak, nonatomic) IBOutlet UILabel *time1;
+@property (weak, nonatomic) IBOutlet UILabel *time2;
+@property (weak, nonatomic) IBOutlet UILabel *time3;
+@property (weak, nonatomic) IBOutlet UILabel *time4;
 
 @property (assign, nonatomic) IBOutlet id<NPTimeOnlineCellDelegate_iPad> delegate;
 @property (strong,nonatomic)NSArray *modelArray;
